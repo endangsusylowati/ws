32798a50e8f21498251aa4ff7e59780dedff9c71
endang susylowati <endangc@gmail.com>
2014-10-15 07:33:48 +0700

    add client.php

---
+++ login/client.php
@@ -0,0 +1,23 @@
+<?php
+	//memlai
+	session
+	session_start(
+	);
+	//panggil library
+	require_once('nusoap/lib/nusoa
+	p.php');
+	//mendefinisikan alamat url service yang disediakan oleh client
+	$client = new
+	soapclient('http://localhost/login/server.php?wsdl',true);
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$result  =  $client->call('login_ws',
+	array('username'=>$username,
+	'password'=>$password));
+	if($result == "Login Berhasil"){
+	$_SESSION['username'] = $username;
+	header ("location:index.php");
+	} else{
+	header ("location:login.php");
+	}
+	?>
